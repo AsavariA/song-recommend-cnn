8c2e7635f083bd64bfbe72294617ce444569c045
Surabhi Chandane <80352794+surabhi175@users.noreply.github.com>
2021-04-03 17:18:36 +0530

    Update main.dart

--- frontend/SongRecommendApp/lib/main.dart
+++ frontend/SongRecommendApp/lib/main.dart
@@ -1,49 +1,51 @@
-import 'package:flutter/material.dart';
-import 'package:myapp/screens/apptheme.dart';
+import 'dart:async';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:myapp/screens/AppStateNotifier.dart';
+import 'package:myapp/screens/config.dart';
+import 'package:myapp/screens/homePage.dart';
+import 'package:myapp/screens/settings.dart';
+import 'package:myapp/screens/swipeScreen.dart';
 import 'package:myapp/screens/themedemo.dart';
 import 'package:myapp/screens/versionScreen.dart';
 import 'package:provider/provider.dart';
-import './screens/homePage.dart';
-import './screens/settings.dart';
-import './screens/swipeScreen.dart';
+import './screens/AppStateNotifier.dart';
+import './screens/apptheme.dart';
 import './screens/forgotPassword.dart';
 import './screens/loginPage.dart';
 import './screens/register.dart';
-import './screens/AppStateNotifier.dart';
+import 'package:flutter/material.dart';
 
+StreamController<bool> isLightTheme = StreamController();
 void main() {
-  runApp(
-    ChangeNotifierProvider<AppStateNotifier>(
-      create:(context)=>
-      AppStateNotifier(),
-      child: MyApp(),
-    )
-  );
+  runApp(MyApp());
 }
 
-class  MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer<AppStateNotifier>(
-      builder: (context, appState, child){
-        return MaterialApp(
-            title: 'Song Recommendation App',
-            debugShowCheckedModeBanner: false,
-            darkTheme: AppTheme.darkTheme,
-            theme: AppTheme.lightTheme,
-            themeMode: appState.isDarkModeOn ? ThemeMode.dark : ThemeMode.light,
-            home: SwipeScreens(),
-            routes:{
-              ForgotPassword.routeName: (ctx) => ForgotPassword(),
-              CreateNewAccount.routeName: (ctx) => CreateNewAccount(),
-              LoginPage.routeName: (ctx) => LoginPage(),
-              HomePage.routeName: (ctx) => HomePage(),
-              Settings.routeName: (ctx) => Settings(),
-              Version.routeName: (ctx) => Version(),
-              ThemeDemo.routeName: (ctx) => ThemeDemo()
-            }
-        );
-      },
-    );
+    return StreamBuilder<bool>(
+        initialData: true,
+        stream: isLightTheme.stream,
+        builder: (context, snapshot) {
+          return MaterialApp(
+              theme: snapshot.data ? ThemeData.light() : ThemeData.dark(),
+              debugShowCheckedModeBanner: false,
+              home: /*Scaffold(
+                  appBar: AppBar(title: Text("Dynamic Theme")),
+                  body: SettingPage())*/
+              SwipeScreens(),
+              routes: {
+                ForgotPassword.routeName: (ctx) => ForgotPassword(),
+                CreateNewAccount.routeName: (ctx) => CreateNewAccount(),
+                LoginPage.routeName: (ctx) => LoginPage(),
+                HomePage.routeName: (ctx) => HomePage(),
+                Settings.routeName: (ctx) => Settings(),
+                Version.routeName: (ctx) => Version(),
+                ThemeDemo.routeName: (ctx) => ThemeDemo(),
+                AppTheme.routeName: (ctx) => AppTheme(),
+              }
+
+          );
+        });
   }
 }
